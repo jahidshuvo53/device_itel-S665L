894b3e65f0759b73d79d714dc0cf538f16a4e3be
jahidshuvo53 <153370216+jahidshuvo53@users.noreply.github.com>
2024-03-14 00:23:32 +0530

    TWRP Bringup

--- twrp_itel-S665L.mk
+++ twrp_itel-S665L.mk
@@ -7,16 +7,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from itel-S665L device
 $(call inherit-product, device/itel/itel-S665L/device.mk)
 
 PRODUCT_DEVICE := itel-S665L
-PRODUCT_NAME := omni_itel-S665L
+PRODUCT_NAME := twrp_itel-S665L
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel S665L
 PRODUCT_MANUFACTURER := itel
