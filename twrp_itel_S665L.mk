1c22e252bf99c41a0e16a2af7e4c0d9b2f0d71a0
jahidshuvo53 <153370216+jahidshuvo53@users.noreply.github.com>
2024-03-14 11:18:01 +0530

    Update and rename twrp_itel-S665L.mk to twrp_itel_S665L.mk

--- twrp_itel_S665L.mk
+++ twrp_itel_S665L.mk
@@ -13,17 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from itel-S665L device
-$(call inherit-product, device/itel/itel-S665L/device.mk)
+$(call inherit-product, device/itel/itel_S665L/device.mk)
 
-PRODUCT_DEVICE := itel-S665L
-PRODUCT_NAME := twrp_itel-S665L
+PRODUCT_DEVICE := itel_S665L
+PRODUCT_NAME := twrp_itel_S665L
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel S665L
 PRODUCT_MANUFACTURER := itel
-
-PRODUCT_GMS_CLIENTID_BASE := android-transsion
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="S665L-user 12 SP1A.210812.016 490 release-keys"
-
-BUILD_FINGERPRINT := Itel/S665L-GL/itel-S665L:12/SP1A.210812.016/GL-20231025V481:user/release-keys
