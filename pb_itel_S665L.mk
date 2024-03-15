26c95b7af45639d9eef1ca89c198f356d61934a8
jahidshuvo53 <153370216+jahidshuvo53@users.noreply.github.com>
2024-03-15 16:45:23 +0530

    Update and rename twrp_itel_S665L.mk to pb_itel_S665L.mk

--- pb_itel_S665L.mk
+++ pb_itel_S665L.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common PBRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from itel-S665L device
 $(call inherit-product, device/itel/itel_S665L/device.mk)
 
 PRODUCT_DEVICE := itel_S665L
-PRODUCT_NAME := twrp_itel_S665L
+PRODUCT_NAME := pb_itel_S665L
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel S665L
 PRODUCT_MANUFACTURER := itel
